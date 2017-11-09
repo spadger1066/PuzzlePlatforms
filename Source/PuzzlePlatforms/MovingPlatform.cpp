8463e30c691916e9ef90300770a3639ebec45aba
RAMPAGE\Stephen <s.maitland@me.com>
2017-11-09 20:35:55 +0000

    Initial commit

---
+++ Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MovingPlatform.h"
+
+AMovingPlatform::AMovingPlatform() {
+	PrimaryActorTick.bCanEverTick = true;
+
+	SetMobility(EComponentMobility::Movable);
+}
+
+void AMovingPlatform::Tick(float DeltaSeconds) {
+	Super::Tick(DeltaSeconds);
+
+	if (HasAuthority()) {
+		FVector Location = GetActorLocation();
+		Location += FVector(Speed * DeltaSeconds, 0, 0);
+		SetActorLocation(Location);
+	}
+}
